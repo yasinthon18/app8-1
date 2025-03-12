02a7069e392227d2696fd334415bf1c2e0bff789
yasinthon18 <152575428+yasinthon18@users.noreply.github.com>
2025-03-12 14:25:34 +0700

    add file form-post.js and edit file index.js
    add file form-post.js and edit file index.js

--- server/index.js
+++ server/index.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const port = 8000
 
-app.get('/',(request, response)=> {
-    response.send(`
-        <!doctype html>
-        <html>
-        <head>
-            <title>Express.js</title>
-        </head>
-        <body>
-            <h3>Welcome to Express.js</h3>
-            <b>Express.js Fast, unopinionated, minimalist <br>
-            web framework for Node.js</b>
-        </body>
-        </html>
-    `)
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static('public'))
+
+app.post('/api/form-post',(request,response) => {
+    let name = request.body.name ||''
+    let email = request.body.email ||''
+    let msg = request.body.message ||''
+
+    let text =`
+    <table border = "1">
+        <caption>ข้อมูลที่ส่งขึ้นไป</caption>
+        <tr><td>ชื่อ:</td><td>${name}</td></tr>
+        <tr><td>อีเมล:</td><td>${email}</td></tr>
+        <tr><td>ข้อความ:</td><td>${msg}</td></tr>
+    </table>
+    `
+
+    response.send(text)
 })
-app.listen(port,() => console.log('Server listening on port'+port))
+
+app.listen(port,() => {
+    console.log('Server listening on port'+port)
+})
